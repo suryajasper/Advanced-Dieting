4c3ec5a24d60c5ce86b6936bb13d57d5c255acfb
shlinky <2004shlok@gmail.com>
2020-01-26 13:39:00 -0800

    update server

--- Advanced_dieting/HealthServer.py
+++ Advanced_dieting/HealthServer.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request, render_template, make_response
+from flask import Flask, escape, request, render_template, make_response, url_for, redirect
 from UserDatabase import UserDatabase
 app = Flask(__name__)
 
@@ -6,11 +6,16 @@ usr_db = None
 
 LOGIN = 'login.html'
 HEALTHDATA = 'healthData.html'
-INDEX = 'index.html'
+INDEX = 'mainPage.html'
+SIGNUP = 'signup.html'
 
 def initialize_vars():
 	global usr_db
 	usr_db = UserDatabase()
+
+@app.route('/')
+def frontPage():
+   return (redirect(url_for('login')))
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -25,7 +30,16 @@ def login():
    else:
       return render_template(LOGIN)
 
-app.route('/index')
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+   if request.method == 'POST':
+      ilist = [request.form['pass'], request.form['gender'], request.form['age'], request.form['height'], request.form['weight']]
+      usr_db.add_user(request.form['user'], ilist)
+      return (redirect(url_for('login')))
+   else:
+      return render_template(SIGNUP)
+
+@app.route('/mainPage')
 def mainBlob():
    return render_template(INDEX)
 
--- Advanced_dieting/UserDatabase.py
+++ Advanced_dieting/UserDatabase.py
@@ -1,6 +1,23 @@
 class UserDatabase:
 	def __init__(self):
-		pass
+		self.ufile = 'usrdata.db'
 
 	def login(self,usr, pss):
+		f = open(self.ufile, 'r')
+		data = f.readlines()
+		f.close()
+		for i in data:
+			if (i.split()[0] == usr):
+				if (i.split()[1] == pss):
+					return True
+				else:
+					return False
+		return False
+
+	def add_user(self, usr, ilist):
+		f = open(self.ufile, 'a')
+		for i in ilist:
+			usr += ' ' + i
+		f.write(usr)
+		f.close()
 		return True
