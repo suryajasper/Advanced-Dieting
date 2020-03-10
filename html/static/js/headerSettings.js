16c9c51c874542ed8d3717d0404d9fdadae50e98
Surya Jasper <suryajasper2004@gmail.com>
2020-03-10 13:48:45 -0700

    You can now sign out

--- html/static/js/headerSettings.js
+++ html/static/js/headerSettings.js
@@ -2,6 +2,8 @@ var profileImage = document.getElementById("profileImg");
 var auth = document.getElementById('auth');
 var notauth = document.getElementById('notauth');
 
+var signout = document.getElementById("signout");
+
 function hideAuth() {
   notauth.style.display = "block";
   auth.style.display = "none";
@@ -10,3 +12,16 @@ function showAuth() {
   auth.style.display = "block";
   notauth.style.display = "none";
 }
+
+function signOut() {
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    hideAuth();
+    window.location = "/index.html"
+  }).catch(function(error) {
+    window.alert(error.message);
+    // An error happened.
+  });
+}
+
+signout.onclick = signOut;
