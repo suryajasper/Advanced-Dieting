9caf6fc93a1f00e3735c3caaa1fdfbd608078f62
Surya Jasper <suryajasper2004@gmail.com>
2020-04-29 23:14:39 -0700

    added client-side code for charts

--- html/client/static/js/viewCharts.js
+++ html/client/static/js/viewCharts.js
@@ -1,71 +1,71 @@
+var socket = io();
+
 var lineChart = document.getElementById("lineChart")
 var pieChart = document.getElementById("pieChart")
 
 initializeFirebase();
 
-var database = firebase.database();
-var rootUser = database.ref("users");
+var nutrients = ['alcohol','caffeine','calcium','calories','carbs','cholesterol','copper','fat','fiber','folate','iron','magnesium','manganese','phosphorus','potassium','protein','saturatedFat','selenium','sodium','sugar','vitaminA','vitaminB1','vitaminB12','vitaminB2','vitaminB3','vitaminB5','vitaminB6','vitaminC','vitaminD','vitaminE','vitaminK','zinc'];
 
-function displayPieGraph(values, chart) {
+function addOptions(select_id, options) {
+  $('#' + select_id).empty();
+  for (var optionText of options) {
+    var newOption = document.createElement('option');
+    newOption.innerHTML = optionText;
+    newOption.value = optionText;
+    document.getElementById(select_id).appendChild(newOption);
+  }
+}
+
+addOptions('data', nutrients);
+
+function displayLineGraph(title, values, chart) {
   var ctx = chart.getContext('2d');
   var myChart = new Chart(ctx, {
-    type: 'pie',
+    type: 'line',
     data: {
-        labels: ["Dairy", "Protein", "Fruit", "Vegetable", "Grain", "Other"],
-        datasets: [{
-            label: 'Food group consumption',
-            data: values,
-            backgroundColor: [
-                'rgba(111, 57, 7, 0.61)',
-                'rgba(249, 0, 0, 0.31)',
-                'rgba(11, 79, 0, 0.56)',
-                'rgba(82, 7, 7, 0.65)',
-                'rgba(255, 246, 124, 0.61)',
-                'rgba(143, 4, 218, 0.61)'
-            ],
-            borderColor: [
-              'rgba(111, 57, 7, 0.61)',
-              'rgba(249, 0, 0, 0.72)',
-              'rgba(11, 79, 0, 0.83)',
-              'rgba(98, 5, 5, 0.84)',
-              'rgba(255, 246, 124, 0.81)',
-              'rgba(166, 0, 255, 0.85)'
-            ],
-            borderWidth: 1
-        }]
+      labels: Object.keys(values),
+      datasets: [{
+        label: title,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75, 192, 192, 0.4)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHitRadius: 10,
+        data: Object.values(values),
+      }]
     },
     options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      legend: {
-        labels: {
-          // This more specific font property overrides the global property
-          fontSize: 30
+        scales: {
+            yAxes: [{
+                stacked: true
+            }]
         }
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero:true
-          }
-        }]
-      }
     }
   });
 }
-function getFromServer() {
-  var userId = firebase.auth().currentUser.uid;
-  firebase.database().ref('/eatHistory/' + userId).once('value').then(function(snapshot) {
+
+function getFromServer(id) {
     var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
     console.log(snapshot.val());
     displayPieGraph(arrayify(snapshot.val()), pieChart);
-  });
 }
 
 firebase.auth().onAuthStateChanged(user => {
   if(user) {
     showAuth();
-    getFromServer();
+    document.getElementById('genGraph').onclick = function(e) {
+      e.preventDefault();
+      displayLineGraph('testChart', {"hi": 1, "hi1": 2,"hi2":  3,"hi3":  4}, document.getElementById('lineChart'));
+      //getFromServer(user.uid);
+    }
   }
 });
-//displayPieGraph([1, 2, 3, 4, 5, 6], pieChart);
