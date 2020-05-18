6a6ff7c3174af931a0a5d2d8e315bd8be33db8f1
Surya Jasper <suryajasper2004@gmail.com>
2020-05-17 23:25:59 -0700

    Added UI for removing from eat history

--- html/client/static/js/eatHistory.js
+++ html/client/static/js/eatHistory.js
@@ -18,15 +18,39 @@ firebase.auth().onAuthStateChanged(function(user) {
     for (var date of Object.keys(history)) {
       var dateheading = document.createElement('h3');
       dateheading.style.display = 'block';
+      dateheading.style.color = 'rgb(0, 0, 0)';
       dateheading.innerHTML = replaceAll(date, ' ', '/');
       parent.appendChild(dateheading);
 
+      var ul = document.createElement('ul');
       for (var food of Object.values(history[date])) {
+        var li = document.createElement('li');
+        li.style.display = 'block';
+
+        var sectDiv = document.createElement('div');
+        sectDiv.classList.add('eatHistorySect');
+
         var p = document.createElement('p');
-        p.style.display = 'block';
+        p.style.color = "rgb(52, 52, 52)";
+        p.style.display = 'inline-block';
+        p.classList.add('alignleft');
         p.innerHTML = food.name;
-        parent.appendChild(p);
+        sectDiv.appendChild(p);
+
+        var deleteButton = document.createElement('button');
+        deleteButton.classList.add('alignright');
+        deleteButton.classList.add('eatHistoryDelete');
+        deleteButton.innerHTML = 'Remove';
+        sectDiv.appendChild(deleteButton);
+
+        var clearDiv = document.createElement('div');
+        clearDiv.style.clear = 'both';
+        sectDiv.appendChild(clearDiv);
+
+        li.appendChild(sectDiv);
+        ul.appendChild(li);
       }
+      parent.appendChild(ul);
     }
   })
 })
