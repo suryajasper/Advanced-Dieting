2d90d1b763a9f0bb9cbaeb6bc4c873e167db293f
Surya Jasper <suryajasper2004@gmail.com>
2020-05-20 20:48:01 -0700

    fixed signup to only include email and password

--- html/client/static/js/signup.js
+++ html/client/static/js/signup.js
@@ -1,16 +1,3 @@
-var email = document.getElementById("email");
-var username = document.getElementById("username");
-var pword = document.getElementById("pword");
-var confirmpword = document.getElementById("confirmpword");
-var gender = document.getElementById("gender");
-var weight = document.getElementById("weight");
-var height = document.getElementById("height");
-var age = document.getElementById("age");
-
-var diseases = document.getElementById("diseases");
-var toReduce = document.getElementById("toReduce");
-var toIncrease = document.getElementById("toIncrease");
-
 var submitButton = document.getElementById("testSubmit");
 
 hideAuth();
@@ -35,13 +22,13 @@ firebase.auth().onAuthStateChanged(user => {
     console.log("if (user)");
     console.log(user.uid);
     rootUser.child(user.uid).set({
-      gender: gender.value,
-      weight: weight.value,
-      height: height.value,
-      age: age.value,
-      diseases: diseases.value,
-      to_reduce: toReduce.value,
-      to_increase: toIncrease.value
+      gender: "",
+      weight: "",
+      height: "",
+      age: "",
+      diseases: "",
+      to_reduce: "",
+      to_increase: ""
     });
     window.location = 'main.html';
   }
